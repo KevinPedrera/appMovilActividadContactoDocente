3fdb3e7730c6753eac206654a6c3f111b037d3a7
KevinPedrera <kevinpedrera@outlook.es>
2025-07-24 00:17:51 -0500

    eje4 2

--- src/typescript/Ejercicio4.tsx
+++ src/typescript/Ejercicio4.tsx
@@ -1,3 +1,11 @@
+// interface
+
+interface Alumno{
+    name: string;
+    edad: number;
+    calificacion: number;
+}
+
 // rafc + tab o enter
 export const Ejercicio4 = () => {
     const alumnos: Alumno[] = [
@@ -21,8 +29,17 @@ export const Ejercicio4 = () => {
     // Funcion para el promedio
     const Promedio = (listaAlumnos: Alumno[]): number => {
         const sumaNotas = listaAlumnos.reduce((total, alumno) => total + alumno.calificacion,0);
+        return sumaNotas / listaAlumnos.length;
     }
+    const promedio= Promedio(alumnos);
   return (
-    <div>Ejercicio4</div>
+    <div>
+        <h3>Promedio de los Alumnos</h3>
+        <h4>Lista de Alumnos:</h4>
+        <ul>
+            
+        </ul>
+
+    </div>
   )
 }
