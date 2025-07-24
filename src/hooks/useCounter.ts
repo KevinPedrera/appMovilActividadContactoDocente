9e1f9815834037ab1f479e72e81567a20597a9fb
KevinPedrera <kevinpedrera@outlook.es>
2025-07-24 00:38:05 -0500

    eje5

---
+++ src/hooks/useCounter.ts
@@ -0,0 +1,13 @@
+// rafc + tab o enter
+import { useState } from "react"
+
+export const useCounter = () => {
+    const [contador, setContador] = useState<number>(0);
+    const gestionarContador = (numero: number): void =>{
+        setContador(contador + numero);
+    }
+  return (
+    contador,
+    gestionarContador
+  )
+}
